fb8cd75e5894306cd592e852d0b60c0b6a61ee2d
janaj87 <wordpress.jana@gmail.com>
2025-12-27 15:43:10 +0100

    init commit

---
+++ src/main/java/net/javaguides/springboot_jutjubic/SpringbootJutjubicApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot_jutjubic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJutjubicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJutjubicApplication.class, args);
+	}
+
+}
