5fced1c032182b46eabb117555346ba701202a22
Vasilisa Petrovic <vasilisapetrovic@icloud.com>
2026-01-04 14:14:48 +0100

    Video uploading

---
+++ src/main/java/net/javaguides/springboot_jutjubic/model/Video.java
@@ -0,0 +1,147 @@
+package net.javaguides.springboot_jutjubic.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "VIDEO_POSTS")
+@NamedQuery(name = "VideoPost.findByUserId",
+        query = "select v from Video v where v.userId=?1")
+public class Video implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false, length = 1000)
+    private String description;
+
+    @ElementCollection
+    @CollectionTable(name = "video_post_tags", joinColumns = @JoinColumn(name = "post_id"))
+    @Column(name = "tag")
+    private List<String> tags;
+
+    @Column(name = "thumbnail_path")
+    private String thumbnailPath;
+
+    @Column(name = "video_path")
+    private String videoPath;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Version
+    private Integer version;
+
+    public Video() {
+        super();
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Video(String title, String description, List<String> tags, Long userId) {
+        this.title = title;
+        this.description = description;
+        this.tags = tags;
+        this.userId = userId;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public String getThumbnailPath() {
+        return thumbnailPath;
+    }
+
+    public void setThumbnailPath(String thumbnailPath) {
+        this.thumbnailPath = thumbnailPath;
+    }
+
+    public String getVideoPath() {
+        return videoPath;
+    }
+
+    public void setVideoPath(String videoPath) {
+        this.videoPath = videoPath;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    @Override
+    public String toString() {
+        return "VideoPost [id=" + id + ", title=" + title + ", userId=" + userId +
+                ", createdAt=" + createdAt + ", version=" + version + "]";
+    }
+}
---
+++ src/main/java/net/javaguides/springboot_jutjubic/service/VideoService.java
@@ -0,0 +1,27 @@
+package net.javaguides.springboot_jutjubic.service;
+
+import net.javaguides.springboot_jutjubic.model.Video;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface VideoService {
+
+    Video save(Video videoPost, MultipartFile thumbnail, MultipartFile video) throws Exception;
+
+    Video update(Video videoPost) throws ObjectOptimisticLockingFailureException;
+
+    void delete(Long id);
+
+    List<Video> findAll();
+
+    Video findById(Long id);
+
+    byte[] getThumbnail(Long id) throws IOException;
+
+    void invalidateThumbnailCache(Long id);
+
+    byte[] getVideoFile(Long id) throws IOException;
+}
