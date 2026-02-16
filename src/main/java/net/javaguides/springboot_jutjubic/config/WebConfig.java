79f70e4a0fa71cbb51ff8e3f6e8cba414e96c3bd
AleksandraIlic03 <anjailic03@gmail.com>
2026-02-16 09:31:54 +0100

    fix: fix webconfig

--- src/main/java/net/javaguides/springboot_jutjubic/config/WebConfig.java
+++ src/main/java/net/javaguides/springboot_jutjubic/config/WebConfig.java
@@ -12,8 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
